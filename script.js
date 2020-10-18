05f1245333cf2d06ec287775a4fe391bf46cc498
williycole <64283810+williycole@users.noreply.github.com>
2020-10-18 15:37:47 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+function myFunction() {
+  var x = document.getElementById("nav-links");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
