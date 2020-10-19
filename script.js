b19ad54c6095e87bb3dda976715f39e703ce960b
williycole <64283810+williycole@users.noreply.github.com>
2020-10-19 12:23:21 -0500

    adding in site counter api.

--- script.js
+++ script.js
@@ -16,12 +16,12 @@ function myFunction() {
 
 // Function for site counter api
 const counting = document.getElementById("#count");
-
+visitUpdate();
 function visitUpdate(){
-  fetch('https://api.countapi.xyz/update/boren-sprite-clone/sprite-clone/?amount=1')
+  fetch('https://api.countapi.xyz/update/coleboren/spriteclone?amount=1')
   .then(res => res.json())
   .then(res => {
     counting.innerHTML = res.value;
   });
 }
-visitUpdate();
+'https://api.countapi.xyz/update/florin-popcom/codepen/?amount=1'
