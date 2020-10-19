2dfa350b1622ad68e6cf203578c7daf91525ca8b
williycole <64283810+williycole@users.noreply.github.com>
2020-10-19 12:06:19 -0500

    adding in site counter api.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-
+// funciton for hamburger nav menu
 function myFunction() {
   var x = document.getElementById("nav-links");
   if (x.style.display === "block") {
@@ -8,3 +8,20 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+
+
+
+
+
+// Function for site counter api
+const counting = document.getElementById("#count");
+
+function visitUpdate(){
+  fetch('https://api.countapi.xyz/update/boren-sprite-clone/sprite-clone/?amount=1')
+  .then(res => res.json())
+  .then(res => {
+    counting.innerHTML = res.value;
+  });
+}
+visitUpdate();
