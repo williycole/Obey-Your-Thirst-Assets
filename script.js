a9289fe3a9d8479cc1f988a08af8db85c9d9aa12
williycole <64283810+williycole@users.noreply.github.com>
2020-10-19 16:04:05 -0500

    Adding in styling for footer in desktop view.

--- script.js
+++ script.js
@@ -10,3 +10,17 @@ function myFunction() {
 }
 
 
+// contact form validation
+function formValidator() {
+  const name = document.getElementById('name');
+  const comments = document.getElementById('comments');
+  const form = document.getElementById('contact-form');
+  const submit = document.getElementById('submit');
+  if(name.value=== '' || name.value == null  && comments.value === '' || comments.value == null){
+      false;
+      alert('Please provide your full name and comments before submitting the form.');
+  }else {
+      alert('Your form as been submited!');
+  }
+}
+
